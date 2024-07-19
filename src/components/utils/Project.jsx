f7c95cf05f302381117ac177f3280e194ac458af
Kaveh Korki <kavehkorki@gmail.com>
2024-07-19 17:05:06 +1000

    update basic skills and project components

--- src/components/utils/Project.jsx
+++ src/components/utils/Project.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaReact, FaNodeJs, FaCss3Alt } from "react-icons/fa"; // Importing some icons as an example
 
 const Project = () => {
   return (
@@ -8,7 +9,7 @@ const Project = () => {
         <img
           src="project-image-url.jpg"
           alt="Project"
-          className="w-full md:w-1/3 rounded-lg mb-4 md:mb-0 md:mr-4"
+          className="w-full md:w-1/3 rounded-lg mb-4 md:mb-0 md:mr-4 transform transition-transform duration-300 hover:scale-105"
         />
         <div>
           <h4 className="text-xl font-semibold mb-2">Project Title</h4>
@@ -16,6 +17,11 @@ const Project = () => {
             This is a brief description of the project. It showcases the main
             features and technologies used.
           </p>
+          <div className="flex space-x-2 mb-2">
+            <FaReact className="text-blue-500" />
+            <FaNodeJs className="text-green-500" />
+            <FaCss3Alt className="text-blue-700" />
+          </div>
           <a
             href="project-link-url"
             className="text-indigo-500 hover:underline"
