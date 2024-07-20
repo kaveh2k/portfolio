3f9bb890ef2619e904d80f0765ef481c3219c353
Kaveh Korki <kavehkorki@gmail.com>
2024-07-20 18:36:24 +1000

    setup Newsletter.jsx

---
+++ src/components/projects/newsletter/Newsletter.jsx
@@ -0,0 +1,44 @@
+import "./Newsletter.css";
+import Card from "./components/Card";
+const dummyData = [
+  {
+    id: 0,
+    text: "Marketing emails",
+    included: false,
+  },
+  {
+    id: 1,
+    text: "Monthly updates about latest products",
+    included: true,
+  },
+  {
+    id: 2,
+    text: "Technology events",
+    included: true,
+  },
+  {
+    id: 3,
+    text: "Best practices in development",
+    included: true,
+  },
+  {
+    id: 4,
+    text: "And much more fun!",
+    included: true,
+  },
+];
+const Newsletter = () => {
+  return (
+    <>
+      <Card
+        title={"Stay Updated!"}
+        description={
+          "join 60,000+ product managers receiving monthly updates on:"
+        }
+        itemList={dummyData}
+      />
+    </>
+  );
+};
+
+export default Newsletter;
