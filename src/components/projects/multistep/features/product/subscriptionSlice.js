c74ef9aab9098d83f208fed7ed28e00b6327504b
Kaveh Korki <kavehkorki@gmail.com>
2024-07-20 13:50:52 +1000

    setup subscriptionSlice

---
+++ src/components/projects/multistep/features/product/subscriptionSlice.js
@@ -0,0 +1,88 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {
+    name: "",
+    email: "",
+    phone: null,
+  },
+  price: {
+    monthly: {
+      arcade: 9,
+      advanced: 12,
+      pro: 15,
+    },
+    yearly: {
+      arcade: 90,
+      advanced: 120,
+      pro: 150,
+    },
+  },
+  isYearly: false,
+  selectedPlan: "arcade",
+  addOns: [
+    {
+      id: 1,
+      title: "Online service",
+      subTitle: "Access to multiplayer games",
+      price: {
+        monthly: 1,
+        yearly: 10,
+      },
+      isChecked: false,
+    },
+    {
+      id: 2,
+      title: "Larger storage",
+      subTitle: "Extra 1TB of cloud save",
+      price: {
+        monthly: 2,
+        yearly: 20,
+      },
+      isChecked: false,
+    },
+    {
+      id: 3,
+      title: "Customizable profile",
+      subTitle: "Custom theme on your profile",
+      price: {
+        monthly: 2,
+        yearly: 20,
+      },
+      isChecked: false,
+    },
+  ],
+  totalPrice: 0,
+};
+
+const subscriptionSlice = createSlice({
+  name: "subscription",
+  initialState,
+  reducers: {
+    setUser: (state, { payload }) => {
+      state.user = payload;
+    },
+    toggleIsYearly: (state) => {
+      state.isYearly = !state.isYearly;
+    },
+    setSelectedPlan: (state, { payload }) => {
+      state.selectedPlan = payload;
+    },
+    setAddOn: (state, { payload }) => {
+      const addOn = state.addOns.find((addOn) => addOn.id === payload);
+      addOn.isChecked = !addOn.isChecked;
+    },
+    setTotalPrice: (state, { payload }) => {
+      state.totalPrice = payload;
+    },
+  },
+});
+
+export default subscriptionSlice.reducer;
+export const {
+  setUser,
+  toggleIsYearly,
+  setSelectedPlan,
+  setAddOn,
+  setTotalPrice,
+} = subscriptionSlice.actions;
