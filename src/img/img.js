a02ddff69f4c0497c1945f7a7fdb5e6fcadbb756
Kaveh Korki <kavehkorki@gmail.com>
2024-07-21 18:23:16 +1000

    setup intro block

--- src/img/img.js
+++ src/img/img.js
@@ -5,12 +5,30 @@ import git from "./gitt.svg";
 import others from "./others.svg";
 import backend from "./backend.svg";
 import fuse from "./fuse.svg";
+import reactjs from "./react.js.svg";
+import web_developer from "./web_developer.svg";
+
+import js from "./js.svg";
+import nextjs from "./next-js.svg";
+import mongodb from "./mongodb.svg";
+import expressjs from "./express-js.png";
+import nodejs from "./nodejs.svg";
+import apollo from "./apollo.svg";
+import redux from "./redux.svg";
+
 import multistep from "./multistep.png";
 import newsletter from "./newsletter.png";
 import dotaz_front from "./dotaz_front.png";
 import sample from "./sample.png";
 
 export {
+  apollo,
+  redux,
+  nextjs,
+  js,
+  mongodb,
+  expressjs,
+  nodejs,
   team,
   design,
   cloud,
@@ -22,4 +40,6 @@ export {
   dotaz_front,
   fuse,
   sample,
+  web_developer,
+  reactjs,
 };
