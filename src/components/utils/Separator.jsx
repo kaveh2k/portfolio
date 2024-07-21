cf132e16f6bdbf1cd343547eb8450747452a0e6e
Kaveh Korki <kavehkorki@gmail.com>
2024-07-22 00:45:16 +1000

    update design

--- src/components/utils/Separator.jsx
+++ src/components/utils/Separator.jsx
@@ -4,9 +4,7 @@ const Separator = ({ text }) => {
   return (
     <div className="flex items-center my-8 w-full max-w-4xl">
       <div className="flex-grow border-t-2 border-dashed border-gray-300"></div>
-      <span className="mx-4 text-gray-500 text-xl font-mono font-semibold">
-        {text}
-      </span>
+      <span className="mx-4 text-gray-500 text-2xl font-mono ">{text}</span>
       <div className="flex-grow border-t-2 border-dashed border-gray-300"></div>
     </div>
   );
