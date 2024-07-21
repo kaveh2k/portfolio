58246f3e6c7d3d29bd26478d6fa8e472650de697
Kaveh Korki <kavehkorki@gmail.com>
2024-07-21 18:44:30 +1000

    update Body

--- src/components/main/Body.jsx
+++ src/components/main/Body.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Project from "../utils/Project";
 
 import Separator from "../utils/Separator";
 
@@ -9,7 +8,7 @@ import RenderProjects from "./projects/RenderProjects";
 
 const Body = () => {
   return (
-    <main className="flex-grow flex flex-col justify-center items-center p-6 bg-gray-100 m-0">
+    <main className="flex-grow flex flex-col justify-center items-center p-6 bg-gray-100">
       <Intro />
       <Separator />
       {/** Projects */}
